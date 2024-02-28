7c69a68af96e10823d150da031dcefa7cd209f91
Zeke Zhang <sean1832725142@gmail.com>
2024-02-29 01:38:03 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Random Key Generator
+
+This is a very simple random key generator. It generates a random key of a given length and prints it to the console.
+
+## Usage
+
+```bash
+randkey <length> [options]
+```
+
+## Options
+
+- `-f, --format <string>`: Format of the key (base64, hex)
+- `-c, --clipboard`: Copy the generated key to the clipboard
+- `-h, --help`: Show help message
+- `-v, --version`: Show version number
---
+++ randkey/Program.cs
@@ -0,0 +1,53 @@
+﻿using TextCopy;
+using System.Security.Cryptography;
+using Cocona;
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CoconaLiteApp.Run((
+                [Argument(Description = "Length of the key in bytes")] int length,
+                [Option('f', Description = "type of output (base64, hex)")] string? format,
+                [Option('c', Description = "Copy result to clipboard")] bool toClipboard) =>
+            {
+                if (string.IsNullOrEmpty(format)) format = "base64";
+                string result = GenerateKey(length, format);
+                System.Console.WriteLine(result);
+                if (toClipboard)
+                {
+                    ClipboardService.SetText(result);
+                }
+            });
+        }
+
+        static string GenerateKey(int length, string type)
+        {
+            // Generate a random key
+            byte[] key = new byte[length];
+            try
+            {
+                RandomNumberGenerator.Create().GetBytes(key);
+
+                // Convert the key to the desired format
+                string result = type switch
+                {
+                    "base64" => System.Convert.ToBase64String(key),
+                    "hex" => System.BitConverter.ToString(key).Replace("-", " "),
+                    _ => throw new System.ArgumentException($"Invalid type: {type}", nameof(type))
+                };
+                return result;
+            }
+            catch (System.ArgumentException)
+            {
+                Console.WriteLine($"Invalid type: {type}. Accepted types are base64 and hex.");
+            }
+            catch (System.Exception)
+            {
+                throw new System.Exception("Failed to generate a random key");
+            }
+            return "";
+        }
+    }
+}
